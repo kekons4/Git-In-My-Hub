f3ecea924a2060fd0971d49313f3d4dbd2913113
kekons4 <kekons4@gmail.com>
2023-09-29 09:38:43 -0400

    initial boiler plate code

---
+++ Git-In-My-Hub/Git-In-My-Hub/Git_In_My_HubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_In_My_HubApp.swift
+//  Git-In-My-Hub
+//
+//  Created by Keon Pourboghrat on 9/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Git_In_My_HubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
